878520c75b815fed49c7d472b535725aa7af38f2
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-03-03 01:00:00 +0000

    Add `export.js`

---
+++ src/components/export.js
@@ -0,0 +1,168 @@
+import prop1 from "../assets/images/prop1.webp";
+import prop2 from "../assets/images/prop2.webp";
+import prop3 from "../assets/images/prop3.webp";
+import prop4 from "../assets/images/prop4.webp";
+import prop5 from "../assets/images/prop5.webp";
+import prop6 from "../assets/images/prop6.webp";
+import user from "../assets/images/user.png";
+import { MdNoteAlt } from "react-icons/md";
+import {
+  FaHome,
+  FaSearch,
+  FaNotesMedical,
+  FaCameraRetro,
+} from "react-icons/fa";
+import { GoLaw } from "react-icons/go";
+
+export const property = [
+  {
+    images: prop1,
+    address: "Ambarkhana, Sylhet",
+    name: "Villa with Amazing View",
+    price: "BDT 210 lac",
+    about:
+      "Beautiful, updated, ground level Co-op apartment home. The home has a spacious living room and a modern kitchen.",
+    bed: 4,
+    bath: 3,
+    area: "250 sq ft",
+    owner: "Akber Uddin",
+  },
+  {
+    images: prop2,
+    address: "Ambarkhana, Sylhet",
+    name: "Townhouse for Sale",
+    price: "BDT 345 lac",
+    about:
+      "Enjoy the ultimate lifestyle in this beautifully designed home. This home is located in a Town named Ambarkhana, Sylhet.",
+    bed: 4,
+    bath: 3,
+    area: "450 sq ft",
+    owner: "Rumon Ahmed",
+  },
+  {
+    images: prop3,
+    address: "Ambarkhana, Sylhet",
+    name: "Duplex sea facing for rent",
+    price: "BDT 652 lac",
+    about:
+      "This home is located in a sea facing building with a beautiful view of the sea. The home has a spacious living room and a modern kitchen.",
+    bed: 4,
+    bath: 3,
+    area: "129 sq ft",
+    owner: "Foysal Mahmud",
+  },
+  {
+    images: prop4,
+    address: "Zindabazar, Sylhet",
+    name: "Villa with Side View",
+    price: "BDT 876 lac",
+    about:
+      "This home outside the city, but only a 15 minute drive to the city. The home has duplex building with a beautiful view of the city.",
+    bed: 4,
+    bath: 3,
+    area: "453 sq ft",
+    owner: "Numan Ahmed",
+  },
+  {
+    images: prop5,
+    address: "Zindabazar, Sylhet",
+    name: "Awesome villa for rent",
+    price: "BDT 341 lac",
+    about:
+      "This home is located in a most important place in the city. Enjoy the ultimate lifestyle in this beautifully designed home.",
+    bed: 4,
+    bath: 3,
+    area: "320 sq ft",
+    owner: "Faruk Hossen",
+  },
+  {
+    images: prop6,
+    address: "Zindabazar, Sylhet",
+    name: "Sea View Villa",
+    price: "BDT 675 lac",
+    about:
+      "Royal view of the city from this home. Large drawing room with attached bath, spacious lounge and drawing room.",
+    bed: 4,
+    bath: 3,
+    area: "340 sq ft",
+    owner: "Joshim Uddin",
+  },
+];
+
+export const service = [
+  {
+    icon: MdNoteAlt,
+    title: "Sell your home",
+    desc: "We sell your home at the best market price",
+  },
+  {
+    icon: FaHome,
+    title: "Home loans",
+    desc: "We offer you free consultancy to get a loan",
+  },
+  {
+    icon: GoLaw,
+    title: "Legal services",
+    desc: "Expert legal help for all related property items",
+  },
+  {
+    icon: FaSearch,
+    title: "Home inspection",
+    desc: "We make sure you get what you were promised",
+  },
+  {
+    icon: FaNotesMedical,
+    title: "Evaluation",
+    desc: "We offer you free evaluation to get a mortgage loan",
+  },
+  {
+    icon: FaCameraRetro,
+    title: "Photoshoot",
+    desc: "We prepare your home visual presentation",
+  },
+];
+
+export const client = [
+  {
+    image: user,
+    name: "Asma Akter",
+    text: "Excellent team!",
+    feedback:
+      "I had a great experience working with the Sarker Real Estate team. They were very professional and helped me find the perfect home for my family. I would highly recommend them to anyone looking to buy a home.",
+  },
+  {
+    image: user,
+    name: "Moni Begum",
+    text: "Very good work",
+    feedback:
+      "I am very happy with the service I received from the Sarker Real Estate team. They were very professional and helped me find the perfect home for my family. I would highly recommend them to anyone looking to buy a home.",
+  },
+  {
+    image: user,
+    name: "Arif Ahmed",
+    text: "Very well",
+    feedback:
+      "Great service! The Sarker Real Estate team helped me find the perfect home for my family. They were very professional and made the process easy. I would highly recommend them to anyone looking to buy a home.",
+  },
+  {
+    image: user,
+    name: "Delwar Rahim",
+    text: "Superb!",
+    feedback:
+      "We had a great experience working with the Sarker Real Estate team. They were very professional and helped us find the perfect home for our family. We would highly recommend them to anyone looking to buy a home.",
+  },
+  {
+    image: user,
+    name: "Ali Ahmed",
+    text: "Marvelous!",
+    feedback:
+      "My experience with the Sarker Real Estate team was excellent. They were very professional and helped me find the perfect home for my family. I would highly recommend them to anyone looking to buy a home.",
+  },
+  {
+    image: user,
+    name: "Syed Abul",
+    text: "Magnificent!",
+    feedback:
+      "Very professional and helpful team. They helped me find the perfect home for my family. I would highly recommend them to anyone looking to buy a home.",
+  },
+];
